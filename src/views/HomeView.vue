ada7d83b9264a189d24c3684dc7da1d0aac94c78
redstar08 <redstar08@qq.com>
2022-10-13 22:38:21 +0800

    feat: update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
 import TheWelcome from '../components/TheWelcome.vue'
+import AnimateView from './AnimateView.vue'
 </script>
 
 <template>
   <main>
     <TheWelcome />
+    <div class="w-full h-100px flex flex-col justify-center items-center fixed top-0 left-0">
+      <AnimateView />
+    </div>
   </main>
 </template>
