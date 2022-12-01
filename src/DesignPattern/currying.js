a356376e236c265200261bcf057ef2a3d9cc0522
redstar08 <redstar08@qq.com>
2022-12-01 22:55:17 +0800

    feat: update currying

--- src/DesignPattern/currying.js
+++ src/DesignPattern/currying.js
@@ -46,3 +46,46 @@ Function.prototype.uncurrying = function () {
 // const uncurrying = (fn, ...args) => {
 //   fn(...args)
 // }
+
+/**
+ * @description: 将函数柯里化的工具函数
+ * @param {Function} fn 待柯里化的函数
+ * @param {array} args 已经接收的参数列表
+ * @return {Function}
+ */
+const currying = function (fn, ...args) {
+  // fn需要的参数个数
+  const len = fn.length
+  // 返回一个函数接收剩余参数
+  return function (...params) {
+    // 拼接已经接收和新接收的参数列表
+    let _args = [...args, ...params]
+    // 如果已经接收的参数个数还不够，继续返回一个新函数接收剩余参数
+    if (_args.length < len) {
+      return currying.call(this, fn, ..._args)
+    }
+    // 参数全部接收完调用原函数
+    return fn.apply(this, _args)
+  }
+}
+
+const curryingFn = currying((a, b, c) => {
+  return a + b + c
+}, 100)
+
+console.log(curryingFn(1)(2))
+
+const trueCurrying = (fn, ...args) => {
+  // 实际的有效参数, fn.length 代表需要的参数个数
+  if (args.length >= fn.length) {
+    return fn(...args)
+  }
+  return (...args2) => {
+    return trueCurrying(fn, ...args, ...args2)
+  }
+}
+
+const trueCurrying1 = trueCurrying((a, b, c) => {
+  return a + b + c
+}, 100)
+console.log(trueCurrying1(200, 300))
